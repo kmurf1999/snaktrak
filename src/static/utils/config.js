3cc2594a7102c2c2ce14d9be7496b72e35505125
kyle murphy <ruffers39@gmail.com>
2017-06-17 01:26:42 +0000

    added https and sw

--- src/static/utils/config.js
+++ src/static/utils/config.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = 'http://snaktrak.io';
+export const SERVER_URL = 'https://snaktrak.io';
 
 // config should use named export as there can be different exports,
 // just need to export default also because of eslint rules
