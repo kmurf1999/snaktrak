7e8605175a45cce89aaebb693605393c21caf4d7
ruffers9 <ruffers39@gmail.com>
2017-07-01 14:41:16 -0400

    prod dev url

--- src/static/utils/config.js
+++ src/static/utils/config.js
@@ -1,6 +1,10 @@
-export const SERVER_URL = 'https://snaktrak.io';
-//export const SERVER_URL = 'http://localhost:8000';
-
+let URL = null;
+if (process.env.NODE_ENV === 'production') {
+  URL = 'https://snaktrak.io';
+} else {
+  URL = 'http://localhost:8000';
+}
+export const SERVER_URL = URL;
 // config should use named export as there can be different exports,
 // just need to export default also because of eslint rules
 export { SERVER_URL as default };
